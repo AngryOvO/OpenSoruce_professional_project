fc2b1c9c67da784c3acce53589fa699fd264d69b
AngryOvO <moonlighthouse99@gmail.com>
2023-04-17 00:11:20 +0900

    HW#6

---
+++ week66/app/src/main/java/com/example/week6_6/second.java
@@ -0,0 +1,28 @@
+package com.example.week6_6;
+import android.os.Bundle;
+        import android.widget.Button;
+        import androidx.appcompat.app.AppCompatActivity;
+
+public class second extends AppCompatActivity {
+
+    private Button btnReturn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+
+        init();
+        initLr();
+    }
+
+    public void init(){
+        btnReturn = findViewById(R.id.btnReturn);
+    }
+
+    public void initLr(){
+        btnReturn.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
